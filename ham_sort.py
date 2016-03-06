54951306eafce3550b591d731777712875b5503a
Stephen Merity <smerity@smerity.com>
2016-03-06 02:50:53 -0800

    ham_sort updated to use batches for improved processing speed and smoother gradients

--- ham_sort.py
+++ ham_sort.py
@@ -11,28 +11,30 @@ from data import create_example
 from ham import HAMOperations, HAMNode, HAMTree
 
 class Config(object):
-  n = 4
+  max_epochs = 100
+  batch_size = 50
+  n = 8
   embed_size = 10
   tree_size = 20
   controller_size = 20
 
 config = Config()
 
-inputs = tf.placeholder(tf.float32, shape=[config.n, config.embed_size], name='Input')
-control = tf.placeholder(tf.float32, shape=[1, config.controller_size], name='Control')
-target = tf.placeholder(tf.float32, shape=[config.embed_size], name='Target')
+inputs = tf.placeholder(tf.float32, shape=[config.batch_size, config.n, config.embed_size], name='Input')
+control = tf.placeholder(tf.float32, shape=[config.batch_size, config.controller_size], name='Control')
+target = tf.placeholder(tf.float32, shape=[config.batch_size, config.embed_size], name='Target')
 
 ham_ops = HAMOperations(config.embed_size, config.tree_size, config.controller_size)
 tree = HAMTree(ham_ops=ham_ops)
 tree.construct(config.n)
 
-values = tf.split(0, config.n, inputs)
+values = [tf.squeeze(x, [1]) for x in tf.split(1, config.n, inputs)]
 for i, val in enumerate(values):
   tree.leaves[i].embed(val)
 tree.refresh()
 
 calculate_predicted = tree.get_output(control)
-calculate_loss = tf.reduce_sum(tf.pow(calculate_predicted - target, 2))
+calculate_loss = tf.reduce_sum(tf.pow(calculate_predicted - target, 2)) / config.batch_size
 
 optimizer = tf.train.AdamOptimizer(0.001)
 train_step = optimizer.minimize(calculate_loss)
@@ -47,26 +49,28 @@ with tf.Session() as session:
   if os.path.exists(saved_weights_fn):
     saver.restore(session, saved_weights_fn)
 
-  epoch = 0
-  while 1:
-    epoch += 1
-    total_examples = 1000
+  # Paper uses 100 epochs with 1000 batches of batch size 50
+  for epoch in xrange(config.max_epochs):
+    total_batches = 1000
     total_accuracy = 0.0
     total_loss = 0.0
-    for i in xrange(total_examples):
-      X, Y = create_example(n=config.n, bit_length=config.embed_size / 2)
-      control_signal = np.zeros([1, config.controller_size], dtype=np.float32)
-      feed = {inputs: X, target: Y[0], control: control_signal}
+    for i in xrange(total_batches):
+      X, Y = [], []
+      for b in xrange(config.batch_size):
+        x, y = create_example(n=config.n, bit_length=config.embed_size / 2)
+        X.append(x)
+        Y.append(y[0])
+      control_signal = np.zeros([config.batch_size, config.controller_size], dtype=np.float32)
+      feed = {inputs: X, target: Y, control: control_signal}
       _, loss, predicted = session.run([train_step, calculate_loss, calculate_predicted], feed_dict=feed)
       ###
-      y = Y[0].astype(int)
-      y_pred = np.rint(predicted).astype(int)[0]
-      #print('True = {}'.format(y))
-      #print('Pred = {}'.format(y_pred))
-      total_accuracy += (y == y_pred).all()
+      for y, y_pred in zip(Y, predicted):
+        y = y.astype(int)
+        y_pred = np.rint(y_pred).astype(int)
+        total_accuracy += (y == y_pred).all()
       total_loss += loss
     print('Epoch = {}'.format(epoch))
-    print('Loss = {}'.format(total_loss / total_examples))
-    print('Accuracy = {}'.format(total_accuracy / total_examples))
+    print('Loss = {}'.format(total_loss / total_batches))
+    print('Accuracy = {}'.format(total_accuracy / (config.batch_size * total_batches)))
     print('=-=')
     saver.save(session, saved_weights_fn)
