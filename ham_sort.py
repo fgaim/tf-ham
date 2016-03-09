0c27faf991ebedb310c15b71850652e0a035497c
Stephen Merity <smerity@smerity.com>
2016-03-09 09:51:34 -0800

    ham_sort: The task is now correctly sorting the input rather than outputting the smallest element - accuracy has fallen (no surprise)

--- ham_sort.py
+++ ham_sort.py
@@ -8,12 +8,13 @@ import numpy as np
 import tensorflow as tf
 
 from data import create_example
-from ham import HAMOperations, HAMNode, HAMTree
+from ham import HAMOperations, HAMTree
 
 tf.app.flags.DEFINE_float('lr', 0.001, 'Learning rate for Adam optimizer')
-tf.app.flags.DEFINE_integer('max_epochs', 100, 'Maximum epochs to train for.')
-tf.app.flags.DEFINE_integer('batch_size', 32, 'Batch size.')
-tf.app.flags.DEFINE_integer('n', 8, 'Size of the input to sort.')
+tf.app.flags.DEFINE_integer('batches_per_epoch', 1000, 'Number of batches per epoch')
+tf.app.flags.DEFINE_integer('max_epochs', 100, 'Maximum epochs to train for')
+tf.app.flags.DEFINE_integer('batch_size', 50, 'Batch size')
+tf.app.flags.DEFINE_integer('n', 4, 'Size of the input to sort')
 tf.app.flags.DEFINE_integer('embed_size', 10, 'Embedding size for input to sort - half key, half value')
 tf.app.flags.DEFINE_integer('tree_size', 20, 'Tree size')
 tf.app.flags.DEFINE_integer('controller_size', 20, 'Controller size')
@@ -24,7 +25,7 @@ config = tf.app.flags.FLAGS
 
 inputs = tf.placeholder(tf.float32, shape=[config.batch_size, config.n, config.embed_size], name='Input')
 control = tf.placeholder(tf.float32, shape=[config.batch_size, config.controller_size], name='Control')
-target = tf.placeholder(tf.float32, shape=[config.batch_size, config.embed_size], name='Target')
+target = tf.placeholder(tf.float32, shape=[config.batch_size, config.n, config.embed_size], name='Target')
 
 ham_ops = HAMOperations(config.embed_size, config.tree_size, config.controller_size)
 tree = HAMTree(ham_ops=ham_ops)
@@ -35,8 +36,11 @@ for i, val in enumerate(values):
   tree.leaves[i].embed(val)
 tree.refresh()
 
-calculate_predicted = tree.get_output(control)
-calculate_loss = tf.reduce_sum(tf.pow(calculate_predicted - target, 2)) / config.batch_size
+calculate_predicted = tf.concat(1, [tree.get_output(control) for _ in xrange(config.n)])
+targets = tf.reshape(target, [config.batch_size, config.n * config.embed_size])
+penalty = lambda x, y: tf.pow(x - y, 2)
+#penalty = lambda x, y: tf.abs(x - y)
+calculate_loss = tf.reduce_sum(penalty(calculate_predicted, targets)) / config.n / config.batch_size
 
 optimizer = tf.train.AdamOptimizer(config.lr)
 train_step = optimizer.minimize(calculate_loss)
@@ -45,6 +49,11 @@ init = tf.initialize_all_variables()
 saver = tf.train.Saver()
 
 with tf.Session() as session:
+  for var in tf.trainable_variables():
+    tf.histogram_summary(var.op.name, var)
+  summary_op = tf.merge_all_summaries()
+  summary_writer = tf.train.SummaryWriter('/tmp/ham-sort/', session.graph_def, flush_secs=5)
+
   session.run(init)
 
   if os.path.exists(config.weights_path):
@@ -52,7 +61,7 @@ with tf.Session() as session:
 
   # Paper uses 100 epochs with 1000 batches of batch size 50
   for epoch in xrange(config.max_epochs):
-    total_batches = 1000
+    total_batches = config.batches_per_epoch
     total_accuracy = 0.0
     total_loss = 0.0
     for i in xrange(total_batches):
@@ -60,19 +69,28 @@ with tf.Session() as session:
       for b in xrange(config.batch_size):
         x, y = create_example(n=config.n, bit_length=config.embed_size / 2)
         X.append(x)
-        Y.append(y[0])
+        Y.append(y)
       control_signal = np.zeros([config.batch_size, config.controller_size], dtype=np.float32)
       feed = {inputs: X, target: Y, control: control_signal}
       _, loss, predicted = session.run([tf.no_op() if config.test else train_step, calculate_loss, calculate_predicted], feed_dict=feed)
       ###
-      for y, y_pred in zip(Y, predicted):
-        y = y.astype(int)
-        y_pred = np.rint(y_pred).astype(int)
+      for b in xrange(config.batch_size):
+        y = Y[b].reshape(config.n * config.embed_size)
+        y_pred = np.rint(predicted[b]).astype(int)
         total_accuracy += (y == y_pred).all()
       total_loss += loss
+    train_acc = total_accuracy / (config.batch_size * total_batches)
+    train_loss = total_loss / total_batches
     print('Epoch = {}'.format(epoch))
-    print('Loss = {}'.format(total_loss / total_batches))
-    print('Accuracy = {}'.format(total_accuracy / (config.batch_size * total_batches)))
+    print('Loss = {}'.format(train_loss))
+    print('Accuracy = {}'.format(train_acc))
     print('=-=')
+    ###
+    summary = tf.Summary()
+    summary.ParseFromString(session.run(summary_op))
+    summary.value.add(tag='TrainAcc', simple_value=float(train_acc))
+    summary.value.add(tag='TrainLoss', simple_value=float(train_loss))
+    summary_writer.add_summary(summary, epoch)
+    ###
     if not config.test:
       saver.save(session, config.weights_path)
